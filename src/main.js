0c8c23189aa06bd589080c8ae201a10335091157
KimKangin <1479244157@qq.com>
2018-10-09 16:48:31 +0800

    首页完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vueRouter from 'vue-router'
+import index from './components/index.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(vueRouter);
+
+const routes = [
+  {
+    path: '/',
+    component: index
+    // redirect: '/index'
+  },{
+    path: '/index',
+    component: index
+  }
+];
+
+const router = new vueRouter({
+  routes
+});
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
